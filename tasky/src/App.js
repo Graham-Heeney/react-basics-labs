10ba1c5fa37c2b6469be350b00dc9a713d1e24f8
Graham heeney <20102466@mail.wit.ie>
2024-10-02 14:29:33 +0100

    Add task: Form added

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
+import AddTaskForm from './components/Form';
+
 
 
 function App() {
@@ -40,6 +42,8 @@ function App() {
 
     />
   ))}
+        <AddTaskForm />
+
     </div>
   
   );
