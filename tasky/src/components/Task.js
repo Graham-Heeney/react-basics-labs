6e406e06ab739edf2ec549713bd85f0ef429de83
Graham heeney <20102466@mail.wit.ie>
2024-10-02 14:25:29 +0100

    Delete a task

--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -7,6 +7,8 @@ const Task = (props) => {
             <p>Due: {props.deadline}</p>
             <p className="taskDescription">{props.description}</p> {/* Use description prop */}
             <button onClick={props.markDone} className='doneButton'>Done</button>
+            <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
+
 
         </div>
     );
